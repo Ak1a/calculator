9a6efb253ce74fc7c1d92d0f2972d1ff248b6ede
Ak1la <nik96@bk.ru>
2017-03-20 17:39:52 +0200

    Начало JSON

--- calculator.php
+++ calculator.php
@@ -26,6 +26,16 @@ class calculator
         $this->csv = $csv;
     }
 
+    /**
+     * calculator constructor.
+     */
+    function __construct()
+    {
+    }
+
+    /**
+     * @param $typeOfwindow
+     */
     private function typeOfWindow($typeOfwindow)
     {
         if (is_string($typeOfwindow)) {
@@ -33,68 +43,90 @@ class calculator
         } else echo "Please enter string";
     }
 
+    /**
+     * @param $height
+     * @return int
+     */
     private function rangeHeight($height)
     {
         $getCSV = $this->getCsv();
         $num = count($getCSV);
         for ($i = 1; $i < $num; $i++) {
-
             $row = $getCSV[$i][0];
             $str = strpos($row, "-");
-            $row = substr($row, $str+1);
-
-            if ($row < $height) {
+            $row = substr($row, $str + 1);
+            if ($row >= $height) {
                 return $i;
             }
         }
     }
 
+    /**
+     * @param $width
+     * @return int
+     */
     private function rangeWidth($width)
     {
         $getCSV = $this->getCsv();
         $num = count($getCSV);
         for ($i = 1; $i < $num; $i++) {
-
             $row = $getCSV[0][$i];
             $str = strpos($row, "-");
-            $row = substr($row, 0, $str);
-
-            if ($row > $width) {
+            $row = substr($row, $str + 1);
+            if ($row >= $width) {
                 return $i;
             }
         }
     }
 
-    public function getPosition ($type, $height, $width){
+    /**
+     * @param $type
+     * @param $height
+     * @param $width
+     * @return mixed
+     */
+    public function getPrice($type, $height, $width)
+    {
         $this->typeOfWindow($type);
         $h = $this->rangeHeight($height);
         $w = $this->rangeWidth($width);
         $csv = $this->getCsv();
-        return $csv [$h] [$w];
-}
+        $res = $csv [$h] [$w];
+        return $res;
+    }
+
+    public function calculate()
+    {
+
+
+    }
+
+    /**
+     *
+     */
+    function __destruct()
+    {
+        // TODO: Implement __destruct() method.
+    }
 }
 
+//
 //$csv = array_map('str_getcsv', file('type_1.csv'));
 //$num = count($csv);
 //$row = $csv [16] [0];
 //$str = strpos($row, "-");
-//$row = substr($row, $str+1);
-//if ($row > 5000){
+//$row = substr($row, $str + 1);
+//if ($row > 5000) {
 //    echo "lol";
-//}else echo "неlol";
+//} else echo "не lol";
 //
 ////var_dump($csv);
-//echo $row;
-//
-//$row = 1;
-//if (($handle = fopen("type_1.csv", "r")) !== FALSE) {
-//    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-//        $num = count($data);
-//        echo "<p> $num полей в строке $row: <br /></p>\n";
-//        $row++;
-//        for ($c = 0; $c < $num; $c++) {
-//            echo $data[$c] . "<br />\n";
-//        }
-//    }
-//    fclose($handle);
-//}
+//echo $row."<br>";
+
+$lol = new calculator();
+
+$type = $_GET["type"];
+$height = $_GET["height"];
+$width = $_GET["width"];
+$var = $lol->getPrice($type, $height, $width);
+echo "<h2>".$var."</h2>";
